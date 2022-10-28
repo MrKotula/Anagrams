4df4e2b4ff8f98aadd1fe6e881b70d1ce3e202ff
Bohdan Bilyk <race1001@ukr.net>
2022-10-28 15:36:33 +0200

    Delete import from book.java

--- DynamicWeb/src/main/java/ua/com/foxminded/dynamicweb/book.java
+++ DynamicWeb/src/main/java/ua/com/foxminded/dynamicweb/book.java
@@ -3,10 +3,8 @@ package ua.com.foxminded.dynamicweb;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
