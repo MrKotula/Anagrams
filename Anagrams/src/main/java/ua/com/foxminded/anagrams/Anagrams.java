ef9faa2d7bad3dad0af40d98c183133907f2e308
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2022-11-04 16:13:53 +0100

    Added input in ukrainian language

--- Anagrams/src/main/java/ua/com/foxminded/anagrams/Anagrams.java
+++ Anagrams/src/main/java/ua/com/foxminded/anagrams/Anagrams.java
@@ -1,22 +1,22 @@
 package ua.com.foxminded.anagrams;
 
 public class Anagrams {
-	
-	public static String stringReverse(String s) {
-		
+
+	public static String stringReverse(String input) {
+
 		StringBuilder result = new StringBuilder();
-		for (String word : s.split(" ")) {
+		for (String word : input.split(" ")) {
 
-			StringBuilder reverse = new StringBuilder(word.replaceAll("[\\W\\d+]", "")).reverse();
-
+			StringBuilder reverse = new StringBuilder(word.replaceAll("[^a-zA-Z_0-9à-ÿÀ-ß¸¨³²]", "")).reverse();
 			for (int i = 0; i < word.length(); i++) {
+
 				if (!Character.isLetter(word.charAt(i))) {
 					reverse.insert(i, word.charAt(i));
 				}
+
 			}
 			result.append(reverse).append(" ");
 		}
 		return result.toString();
 	}
 }
-
