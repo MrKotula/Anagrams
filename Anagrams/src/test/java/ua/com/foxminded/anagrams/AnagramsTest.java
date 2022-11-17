2f4ba7bcb3c0209c46b494f8401731550044b71f
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2022-11-17 07:55:27 +0100

    Changed method checkNullTest

--- Anagrams/src/test/java/ua/com/foxminded/anagrams/AnagramsTest.java
+++ Anagrams/src/test/java/ua/com/foxminded/anagrams/AnagramsTest.java
@@ -1,58 +1,61 @@
 package ua.com.foxminded.anagrams;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AnagramsTest {
 
-    @Test
-    void checkNullTest() {
-	assertNotNull(Anagrams.stringReverse(""));
-    }
+	@Test
+	void checkNullTest() {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Anagrams.stringReverse(null);
+		});
+	}
 
-    @Test
-    void checkEmptyStringTest() {
-	assertEquals("", Anagrams.stringReverse(""));
-    }
+	@Test
+	void checkEmptyStringTest() {
+		assertEquals("", Anagrams.stringReverse(""));
+	}
 
-    @Test
-    void checkSingleSpaceTest() {
-	assertEquals(" ", Anagrams.stringReverse(" "));
-    }
+	@Test
+	void checkSingleSpaceTest() {
+		assertEquals(" ", Anagrams.stringReverse(" "));
+	}
 
-    @Test
-    void checkSeveralSpacesTest() {
-	assertEquals("  ", Anagrams.stringReverse("  "));
-    }
+	@Test
+	void checkSeveralSpacesTest() {
+		assertEquals("  ", Anagrams.stringReverse("  "));
+	}
 
-    @Test
-    void checkSingleCharacterTest() {
-	assertEquals("A", Anagrams.stringReverse("A"));
-    }
+	@Test
+	void checkSingleCharacterTest() {
+		assertEquals("A", Anagrams.stringReverse("A"));
+	}
 
-    @Test
-    void checkMultipleSameLetterTest() {
-	assertEquals("aaa", Anagrams.stringReverse("aaa"));
-    }
+	@Test
+	void checkMultipleSameLetterTest() {
+		assertEquals("aaa", Anagrams.stringReverse("aaa"));
+	}
 
-    @Test
-    void checkLettersInCasesTest() {
-	assertEquals("bBaAaA", Anagrams.stringReverse("AaAaBb"));
-    }
+	@Test
+	void checkLettersInCasesTest() {
+		assertEquals("bBaAaA", Anagrams.stringReverse("AaAaBb"));
+	}
 
-    @Test
-    void checkWordTest() {
-	assertEquals("olleH", Anagrams.stringReverse("Hello"));
-    }
+	@Test
+	void checkWordTest() {
+		assertEquals("olleH", Anagrams.stringReverse("Hello"));
+	}
 
-    @Test
-    void checkOnlySymbolTest() {
-	assertEquals("@!$", Anagrams.stringReverse("@!$"));
-    }
+	@Test
+	void checkOnlySymbolTest() {
+		assertEquals("@!$", Anagrams.stringReverse("@!$"));
+	}
 
-    @Test
-    void checkSeveralWordsTest() {
-	assertEquals("olleH ym dneirf", Anagrams.stringReverse("Hello my friend"));
-    }
+	@Test
+	void checkSeveralWordsTest() {
+		assertEquals("olleH ym dneirf", Anagrams.stringReverse("Hello my friend"));
+	}
 
 }
